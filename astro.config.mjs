af3ffd47e02f3f4e92d7665af8d4b0b3cd41e044
Peter Chen <13093023233@163.com>
2022-11-26 22:11:04 +0800

    fix: re run action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
