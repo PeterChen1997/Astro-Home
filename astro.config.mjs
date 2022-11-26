a8bd92b5a4147b9214493a34e07c0dfbcae4774b
Peter Chen <13093023233@163.com>
2022-11-26 16:44:27 +0800

    feat: support toggle theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
+
+// https://astro.build/config
+import image from '@astrojs/image'
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
-});
+  site: 'https://blog.peterchen97.cn',
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
+})
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '龙场茶室'
+export const SITE_DESCRIPTION =
+  '练习时长三年半的前端开发小白，这是一些小作文，欢迎交流'
+
+export const HOMEPAGE_URL = 'https://blog.peterchen97.cn'
