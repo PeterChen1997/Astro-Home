139a987c4d23ae2a62bc9e19d1943b8ea4c5fda3
Peter Chen <13093023233@163.com>
2023-07-08 12:20:30 +0800

    fix: fix logic

--- src/components/ViewedCount.tsx
+++ src/components/ViewedCount.tsx
@@ -1,16 +1,18 @@
 import useAxios from 'axios-hooks'
-import { isClientSide } from '../utils'
 
 const ViewedCount = ({ postId }: { postId: string }) => {
-  const [{ data }] = isClientSide()
-    ? useAxios({
-        url: 'https://eastasia.azure.data.mongodb-api.com/app/application-0-etshh/endpoint/article/view',
-        method: 'POST',
-        data: {
-          postId
-        }
-      })
-    : ([{}, () => {}] as any)
+  const [{ data }] = useAxios(
+    {
+      url: 'https://eastasia.azure.data.mongodb-api.com/app/application-0-etshh/endpoint/article/view',
+      method: 'POST',
+      data: {
+        postId
+      }
+    },
+    {
+      manual: true
+    }
+  )
   const viewedCount = data?.viewCount
   console.log(data)
 
