a2818901b189837ed99c595345157da01fae2286
Peter Chen <13093023233@163.com>
2023-12-10 21:21:31 +0800

    feat: remove DS_store

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,4 +2,6 @@
 # sudo yarn build
 # 需要删除缓存
 # ~/Downloads/qshell qupload2 --src-dir=dist --bucket=peter-blog --overwrite --thread-count 100
+# 不删除缓存
+# ~/Downloads/qshell qupload2 --src-dir=dist --bucket=peter-blog --thread-count 100
 # ~/Downloads/qshell cdnrefresh -i ./scripts/refresh.txt
