930dac5704154a6ff3844398e773daf2b52c3e3c
Peter Chen <13093023233@163.com>
2023-12-26 19:52:56 +0800

    feat: add links

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,8 @@
 # rm -rf ./dist
 # sudo yarn build
+
+# rm -rf /Users/peterchen/.qshell/users/晨阳/qupload2
+
 # 需要删除缓存
 # ~/Downloads/qshell qupload2 --src-dir=dist --bucket=peter-blog --overwrite --thread-count 100
 # 不删除缓存
