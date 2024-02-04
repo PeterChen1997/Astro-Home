9042974e38068bbce55f51cecfbc1c4a22ee04f2
PeterChen1997 <13093023233@163.com>
2024-02-04 09:16:53 +0000

    feat: add storybook

---
+++ src/components/DropdownMenu.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import DropdownMenu from './DropdownMenu';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Components/DropdownMenu',
+  component: DropdownMenu,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof DropdownMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Open: Story = {
+  args: {
+    open: true,
+    label: 'DropdownMenu',
+  },
+};
+export const Normal: Story = {
+  args: {
+    label: 'DropdownMenu',
+  },
+};
--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 import { IoMenu } from 'react-icons/io5/index.js'
 import DropdownMenuItem from './DropdownMenuItem'
 
-export default function DropdownMenu() {
+export default function DropdownMenu({open}: {open?: boolean}) {
   const menus = [
     {
       key: 'posts',
@@ -38,7 +38,7 @@ export default function DropdownMenu() {
   ]
 
   return (
-    <Menu as="div" className="relative inline-block text-left block md:hidden">
+    <Menu as="div" className="relative inline-block text-left block md:hidden" __demoMode={open}>
       <div>
         <Menu.Button
           className="inline-flex justify-center rounded-md border border-zinc-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-orange-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
