b7afe52cff77a543eb89107fb4217fe8026021a4
PeterChen1997 <13093023233@163.com>
2024-02-04 09:23:03 +0000

    feat: add docs

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -57,7 +57,7 @@ export default function DropdownMenu({open}: {open?: boolean}) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 bg-orange-50 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-zinc-400 dark:divide-zinc-700">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-zinc-400 dark:divide-zinc-700">
           <div className="py-1">
             {/* <div className="px-3 py-2 uppercase font-bold text-xs">
               Categories
